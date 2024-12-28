3d7877cf9dd1ea7f35a1cd90a607f161291f9d43
peterboro <p.njuguna@edulink.ac.ke>
2024-12-28 22:25:27 +0300

    Modified Header

--- saas_landing_page/src/sections/Header.jsx
+++ saas_landing_page/src/sections/Header.jsx
@@ -1,10 +1,26 @@
+import { Link as LinkScroll } from 'react-scroll';
+
+const NavLink = () => (
+  <LinkScroll></LinkScroll>
+)
+
 const Header = () => {
   return (
-    <header>
-      <div>
-        <a>
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
+
+        <div className="w-full border-2 border-amber-400">
+          <nav>
+            <ul className="flex max-lg:block max-lg:px-12">
+              <li className="nav-li">
+                <NavLink title="features" />
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   )

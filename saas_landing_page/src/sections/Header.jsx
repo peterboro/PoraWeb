80dfe9e97c08b198feafab45df2bfdd99d97b9c6
peterboro <p.njuguna@edulink.ac.ke>
2025-01-02 01:19:10 +0300

    Enhance Header component with responsive navigation and toggle button

--- saas_landing_page/src/sections/Header.jsx
+++ saas_landing_page/src/sections/Header.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ( { title }) => (
-  <LinkScroll>
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500
+  cursor-pointer hove:text-p1 max-lg:my-4 max-lg:h5">
   {title}
   </LinkScroll>
 )
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
       <div className="container flex h-14 items-center max-lg:px-5">
@@ -14,8 +18,10 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
-        <div className="w-full border-2 border-amber-400">
-          <nav>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div className="max-lg:relative max-lg:flex max-lg:flex-col
+        max-lg:min-h screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+          <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
             <ul className="flex max-lg:block max-lg:px-12">
               <li className="nav-li">
                 <NavLink title="features" />
@@ -25,12 +31,27 @@ const Header = () => {
 
               <li className="nav-logo">
                 <LinkScroll>
-                <img src="/images/xora.svg" width={160} height={55} />
+                <img src="/images/xora.svg" width={160} height={55} alt="logo" />
                 </LinkScroll>
               </li>
+
+              <li className="nav-li">
+                <NavLink title="faq" />
+                <div className="dot" />
+                <NavLink title="download" />
+              </li>
+
+
             </ul>
           </nav>
         </div>
+      </div>
+
+      <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center" 
+      onClick={() => setIsOpen((prevState) => !prevState)}>
+      <img src={`/images/${isOpen ? "close" : "magic"}.svg`} alt="magic"
+      className="size-1/2 object-contain" />
+      </button>
       </div>
     </header>
   )

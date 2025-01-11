eff8b6b45b10a258f8af12a1ef8d778b71fde962
peterboro <ptahbn@gmail.com>
2025-01-11 15:34:35 +0300

    Export features constant and update Features section to render feature titles dynamically

--- saas_landing_page/src/constants/index.js
+++ saas_landing_page/src/constants/index.js
@@ -1,4 +1,4 @@
-const features = [
+export const features = [
   {
     id: '0',
     icon: '/images/feature-1.png',
@@ -6,6 +6,20 @@ const features = [
     title: 'FEATURE 1',
     text: 'TEXT 1',
     button: {
+      icon: '/images/magictouch.svg',
+      title: 'Watch the Demo'
+      
+    }
+  },
+  {
+    id: '1',
+    icon: '/images/feature-2.png',
+    caption: 'DEMO CAPTION',
+    title: 'FEATURE 2',
+    text: 'TEXT 2',
+    button: {
+      icon: '/images/magictouch.svg',
+      title: 'Watch the Demo'
       
     }
   }
--- saas_landing_page/src/sections/Features.jsx
+++ saas_landing_page/src/sections/Features.jsx
@@ -1,11 +1,29 @@
 import { Element } from "react-scroll"
+import { features } from "../constants/index"
 
 const Features = () => {
   return <section>
     <Element name="features">
       <div className="container">
-        <div className="relative flex">
-          {}
+        <div className="relative flex 
+        md:flex-wrap 
+        flex-nowrap 
+        border-2 
+        border-s3 rounded-7xl
+        md:overflow-hidden
+        max-md:flex-col
+        feature-after md:g7
+        md-md:border-none
+        max-md:rounded-none max-md:gap-3">
+          {features.map((feature) => (
+            <div key={feature.id} 
+            className="relative z-2 md:px-10 px-5
+            md:pb-10 pb-5 flex-50
+            max-md:g7 max-md:border-2 max-md:border-s3
+            max-md:rounded-3xl max-md:flex-320">
+              {feature.title}
+            </div>
+          ))}
         </div>
       </div>
 

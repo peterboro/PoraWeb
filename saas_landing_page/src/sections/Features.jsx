ab4298bf80d74b4df92597159fd6c5b88289fe36
peterboro <ptahbn@gmail.com>
2025-01-13 23:53:00 +0300

    Refactor Features section to destructure feature properties and improve rendering

--- saas_landing_page/src/sections/Features.jsx
+++ saas_landing_page/src/sections/Features.jsx
@@ -15,13 +15,37 @@ const Features = () => {
         feature-after md:g7
         md-md:border-none
         max-md:rounded-none max-md:gap-3">
-          {features.map((feature) => (
-            <div key={feature.id} 
+          {features.map(({
+            id,
+            icon,
+            caption,
+            title,
+            text,
+            button
+          }) => (
+            <div 
+            key={id} 
             className="relative z-2 md:px-10 px-5
             md:pb-10 pb-5 flex-50
             max-md:g7 max-md:border-2 max-md:border-s3
             max-md:rounded-3xl max-md:flex-320">
-              {feature.title}
+              <div className = "w-full flex justify-start items-start">
+                <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
+                  <div className="w-0.5 h-16 bg-s3"/>
+                  <img 
+                  src={icon} 
+                  className="size-28 object-contain" 
+                  alt={title}
+                  />
+                </div>
+              </div>
+
+              <p className="caption mb-5 max-md:mb-6">
+                {caption}
+              </p>
+              <p className="caption mb-5 max-md:mb-6">
+                {title}
+              </p>
             </div>
           ))}
         </div>
